86d8d4ee84ab5c98f39fdc18f072ec77bb555a7f
sebtechni <68927207+sebtechni@users.noreply.github.com>
2021-01-14 12:02:27 -0500

    Add files via upload

---
+++ rapport-qc.go
@@ -0,0 +1,23 @@
+package lib
+
+import (
+	"fmt"
+	// "encoding/xml"
+	// "time"	
+	// "regexp"		
+	// "strings"
+	// "xlsx"	
+	//"os"
+	//"strconv"
+	// "path"
+	//"encoding/json"	
+	//"io/ioutil"
+	
+	//"os/exec"
+)
+
+func main() {
+	fmt.Println("hello")
+}
+
+
---
+++ rest.go
@@ -0,0 +1,26 @@
+package facest
+
+import (
+    "net/http"
+    "time"
+)
+
+const (
+    BaseURLV1 = " http://baton-2:8080/Baton/api/tasks/"
+)
+
+type Client struct {
+    BaseURL    string
+    apiKey     string
+    HTTPClient *http.Client
+}
+
+func NewClient(apiKey string) *Client {
+    return &Client{
+        BaseURL: BaseURLV1,
+        apiKey:  apiKey,
+        HTTPClient: &http.Client{
+            Timeout: time.Minute,
+        },
+    }
+}
